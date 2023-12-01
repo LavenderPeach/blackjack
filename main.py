7b87dd1f1583402240fcea9d1502e3a6e5cce490
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-11-30 21:52:40 -0500

    added an outcome function that outputs the result of the game to the user.
    added a computer limit that causes the computer to stop drawing once
    their total is above 17. Will actually switch that to <=, as that is
    more aligned with the rules of blackjack.
    
    created a blackjack method towards the beginning of the class that can
    allow the game to play in a certain order.

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ class Game:
         self.user_hand = None
         self.computer_hand = None
 
+    def blackjack(self):
+        self.reset()
+        self.draw_opening()
+        self.print_hands()
+        self.hit()
+        self.computer_limit()
+        self.outcome()
+
     def reset(self):
         self.computer_hand = []
         self.user_hand = []
@@ -19,27 +27,47 @@ class Game:
         while continue_game and not self.user_bust():
           next_move = input("Would you like to draw another card? Type 'Yes' or 'No'")
           if next_move == 'Yes':
-              self.user_draws
+              self.user_draws()
           else:
               continue_game = False
     
     def user_draws(self):
         draw(self.user_hand)
 
-        if self.user_aces and self.user_bust():
-            self.switch_ace
+        if self.user_aces() and self.user_bust():
+            self.switch_ace()
 
     def user_aces(self):
       return 11 in self.user_hand
     
     def user_bust(self):
-        if sum(self.user_hand > 21):
-            print('Busted!!')
+        return sum(self.user_hand) > 21
 
     def switch_ace(self):
           self.user_hand.remove(11)
           self.user_hand.append(1)
     
+    def computer_limit(self):
+        if not self.user_bust():
+            while sum(self.computer_hand) < 17:
+                draw(self.computer_hand)
+        return self.computer_hand
+    def outcome(self):
+        if self.user_bust():
+            print("Bust!!!")
+        elif sum(self.user_hand) == sum(self.computer_hand):
+            print("Draw!")
+        elif sum(self.user_hand) < sum(self.computer_hand) <= 21:
+            print("Dealer wins!")
+        elif sum (self.user_hand) == 21 and sum(self.computer_hand) != 21:
+            print("Blackjack!")
+        else:
+            print("You win!")
+    
+    def print_hands(self):
+        print(f'User Hand: {self.user_hand}')
+        print(f'Computer Hand {self.computer_hand}')
+
     def draw_opening(self):
       for i in range(2):
           draw(self.user_hand)
