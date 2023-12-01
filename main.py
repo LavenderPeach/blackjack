ce110a1d274cc77d49268190566352b8d47d93dc
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-11-30 20:38:48 -0500

    created class "Game" that will house methods to run the game.
    created an initilializer method and a draw method for the opening round.

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ def draw(hand):
     card_values = (11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
     new_card = random.choice(card_values)
     hand.append(new_card)
+
+class Game:
+    def __init__(self):
+        self.user_hand = None
+        self.computer_hand = None
+
+    def draw_opening(self):
+      for i in range(2):
+          draw(self.user_hand)
+          draw(self.computer_hand)
