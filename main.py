635feb64f3b76d0be5a1f49eb833e677a729d1f0
Cade <134173296+LavenderPeach@users.noreply.github.com>
2023-11-30 20:54:23 -0500

    added methods for key functions of the game.
    added a 'reset' method that allows the computer and user's hands to be
    empty.
    
    added an ace function that changes the value from 11 to 1 based on the
    user's score.
    
    created a draw function that allows the user to draw more cards after
    the inital round.

--- main.py
+++ main.py
@@ -10,6 +10,27 @@ class Game:
         self.user_hand = None
         self.computer_hand = None
 
+    def reset(self):
+        self.computer_hand = []
+        self.user_hand = []
+    
+    def user_draws(self):
+        draw(self.user_hand)
+
+        if self.user_aces and self.user_bust():
+            self.switch_ace
+
+    def user_aces(self):
+      return 11 in self.user_hand
+    
+    def user_bust(self):
+        if sum(self.user_hand > 21):
+            print('Busted!!')
+
+    def switch_ace(self):
+          self.user_hand.remove(11)
+          self.user_hand.append(1)
+    
     def draw_opening(self):
       for i in range(2):
           draw(self.user_hand)
